47f537a24acf50d355011f065e274e3fa27e987c
陈元 <chenyuanluck@163.com>
2026-01-19 17:27:35 +0800

    修复货币单位

--- web/src/components/table/usage-logs/utils/index.jsx
+++ web/src/components/table/usage-logs/utils/index.jsx
@@ -1,5 +1,9 @@
+import { getCurrencyConfig } from '@/helpers/index.js';
+
 export * from './fix';
 
 export const renderVideoRatioInfo = (info) => {
-  return `价格: $${info.price} * 分组倍率: ${info.group_ratio} * 秒数: ${info.duration}`;
+  // 获取货币配置
+  const { symbol } = getCurrencyConfig();
+  return `价格: ${symbol}${info.price} * 分组倍率: ${info.group_ratio} * 秒数: ${info.duration}`;
 };
